02af86a830d13602f01e6e1a427b3e35775e8264
aayusiwa <84080903+aayusiwa@users.noreply.github.com>
2021-06-10 19:46:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,24 @@
-# import modules 
 from tkinter import *
-from lyrics_extractor import SongLyrics 
+from lyrics_extractor import SongLyrics
 import os
 from details import GCS_API_KEY
 from details import GCS_ENGINE_ID
-# user defined funtion 
-def get_lyrics(): 
-    global result
-    global s
-    extract_lyrics = SongLyrics(GCS_API_KEY,GCS_ENGINE_ID) 
-    s=e.get()
-    temp = extract_lyrics.get_lyrics(str(s)) 
-    result = temp['lyrics']
 def appenfile(fname,content):
     f=open(fname,"w+")
     f.write(content)
     f.close()
     os.startfile(fname)
-# object of tkinter 
-# and background set to light grey 
-master = Tk() 
-master.configure(bg='light grey') 
-# Variable Classes in tkinter 
-# Creating label for each information 
-# name using widget Label 
-Label(master, text="Enter name of song and artist: ", 
-    bg="light grey").grid(row=0, sticky=W) 
-# Creating lebel for class variable 
-# name using widget Entry 
-e = Entry(master, width=50) 
-e.grid(row=0, column=1) 
-# creating a button using the widget 
-b = Button(master, text="Show", 
-        command=lambda:[get_lyrics(),appenfile('%s.txt'%e.get().title(),res)], bg="Blue") 
-b.grid(row=0, column=2, columnspan=2, 
-    rowspan=2, padx=5, pady=5,) 
+def get_lyrics():
+    extract_lyrics = SongLyrics(str(GCS_API_KEY),str(GCS_ENGINE_ID))
+    s=e.get()
+    temp = extract_lyrics.get_lyrics(str(s))
+    result = temp['lyrics']
+    appenfile(('%s.txt'%(e.get().title())),result)
+master = Tk()
+master.configure(bg='light grey')
+Label(master, text="Enter name of song and artist: ",bg="light grey").grid(row=0, sticky=W)
+e = Entry(master, width=50)
+e.grid(row=0, column=1)
+b = Button(master, text="Show",command=lambda:[get_lyrics()], bg="Blue")
+b.grid(row=0, column=2, columnspan=2,rowspan=2, padx=5, pady=5,)
 mainloop()
